36c1f54c6f143876b55c1f8f90f162d16835cf46
Nick Reid <nickreid@nickreid.com>
2015-12-02 16:55:34 -0800

    made gulp js compile both bower and source js files together

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -7,11 +7,20 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var lib = require('bower-files')();
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
 // Static: Compress JS files into motech.js
 gulp.task('js', function () {
-    gulp.src(lib.ext('js').files)
-        .pipe(concat('motech.min.js'))
+    var files = lib.ext('js').files; // libraries from bower
+    files.push('src/js/*.js'); // common files
+
+    gulp.src(files)
+        .pipe(sourcemaps.init())
+          .pipe(concat('motech.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/js'))
         .pipe(uglify())
+        .pipe(rename('motech.min.js'))
         .pipe(gulp.dest('build/js'));
 });
