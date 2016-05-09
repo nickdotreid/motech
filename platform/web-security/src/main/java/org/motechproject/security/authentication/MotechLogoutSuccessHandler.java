3341e318e7155cdcbe973037877a35e6034761bc
Nick Reid <nickreid@nickreid.com>
2016-05-08 22:02:53 -0700

    MOTECH-2124 Added Ajax-able logout method
    Added switch to logout method to check for Ajax headers, and if so write
    'success' to the browser.

--- platform/web-security/src/main/java/org/motechproject/security/authentication/MotechLogoutSuccessHandler.java
+++ platform/web-security/src/main/java/org/motechproject/security/authentication/MotechLogoutSuccessHandler.java
@@ -1,5 +1,7 @@
 package org.motechproject.security.authentication;
 
+import org.motechproject.commons.api.json.MotechJsonMessage;
+import org.motechproject.osgi.web.extension.HttpRequestEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * A logout handler for logging users that log out from MOTECH.
@@ -21,5 +24,14 @@ public class MotechLogoutSuccessHandler implements LogoutHandler {
     public void logout(HttpServletRequest request, HttpServletResponse response,
                        Authentication authentication) {
         LOGGER.info("User {} logged out", authentication == null ? "not_authenticated" : authentication.getName());
+        if(HttpRequestEnvironment.isAjax(request)){
+            response.setHeader("Content-Type", "application/json");
+            MotechJsonMessage message = new MotechJsonMessage("SUCCESS");
+            try{
+                response.getWriter().write(message.toJson());
+            } catch(IOException exception){
+                LOGGER.error("Errror logging out User {} with Ajax", authentication == null ? "not_authenticated" : authentication.getName());
+            }
+        }
     }
 }
